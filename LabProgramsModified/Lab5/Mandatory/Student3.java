7de8acf2ad83be179c6ce5ddaf8c6d74ddce6b03
sripilla <120908523+sripilla@users.noreply.github.com>
2024-09-08 12:27:32 +0100

    Update Student3.java

--- LabProgramsModified/Lab5/Mandatory/Student3.java
+++ LabProgramsModified/Lab5/Mandatory/Student3.java
@@ -4,7 +4,8 @@ Add member functions to the above code that perform the following operations
 a) List all the students whose name starts with a particular character.
 b) List all the student names containing a particular sub string.
 c) Change the full name in the object to name with just initials and family name. 
-For example, Prakash Kalingrao Aithal must be changed to P.K.Aithal and store it in the object. Display modified objects.
+For example, Prakash Kalingrao Aithal must be changed to P.K.Aithal and store it in the object. 
+Display modified objects.
 */
 import java.util.ArrayList;
 import java.util.Collections;
