e4260acc6088a91886360b3a13f86c6995edbaac
sripilla <120908523+sripilla@users.noreply.github.com>
2024-09-08 04:15:58 +0100

    Update Counter.java

--- LabProgramsModified/Lab4/Mandatory/Counter.java
+++ LabProgramsModified/Lab4/Mandatory/Counter.java
@@ -1,5 +1,8 @@
 /*
-Create a class called Counter that contains a static data member to count the number of Counter objects  being created. Also define a static member function called showCount() which displays the number of objects created at any given point of time. Illustrate this
+Create a class called Counter that contains a static data member to count the number of Counter objects 
+being created. 
+Also define a static member function called showCount() which displays the number of objects created 
+at any given point of time. Illustrate this
 */
 // Define the Counter class
 class Counter {
