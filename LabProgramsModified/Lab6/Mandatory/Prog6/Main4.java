0d95c60c5b09ae32b5a848ed51c86ff77e1739cc
sripilla <120908523+sripilla@users.noreply.github.com>
2024-09-11 01:16:21 +0100

    Update Main4.java

--- LabProgramsModified/Lab6/Mandatory/Prog6/Main4.java
+++ LabProgramsModified/Lab6/Mandatory/Prog6/Main4.java
@@ -1,7 +1,12 @@
 /* Design a base class called Student with the following 2 fields
 i) Name
 ii) Id
-Derive 2 classes called Sports and Exam from the Student base class. Class Sports has a field called s_grade and class Exam has a field e_grade which are integer fields. Derive a class called Results which inherit from Sports and Exam. This class has a character array or string field to represent the final result. Also it has a method called display which can be used to display the final result. Illustrate the usage of these classes in main.*/
+Derive 2 classes called Sports and Exam from the Student base class. 
+Class Sports has a field called s_grade and class Exam has a field e_grade which are integer fields. 
+Derive a class called Results which inherit from Sports and Exam. 
+This class has a character array or string field to represent the final result. 
+Also it has a method called display which can be used to display the final result. 
+Illustrate the usage of these classes in main.*/
 
 // Base class Student
 class Student {
