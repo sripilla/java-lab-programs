ed774f12090cdeda3bfe6bd8674f48c1bdd1d8bb
sripilla <120908523+sripilla@users.noreply.github.com>
2024-09-11 01:49:20 +0100

    Update Employee1.java

--- LabProgramsModified/Lab6/Mandatory/Prog2/Employee1.java
+++ LabProgramsModified/Lab6/Mandatory/Prog2/Employee1.java
@@ -1,4 +1,11 @@
-/*To the already defined EMPLOYEE1 class, add two subclasses. FullTimeEmp and PartTimeEmp. Add the following data Members and Member Functions to the PartTimeEmp class. hoursWorked(int): Represents the number of hours worked by the part-time employee. hourly-rate(double, static and final): Represents the hourly rate at which the part-time employee is paid. The calculateSalary(): overrides the calculateSalary() method of the base class to calculate the salary of the part-time employee based on the hours worked and hourly rate. The displayEmployeeDetails(): Overrides the displayEmplyeeDetails() methods of the base class to display the part-time employee's details, including the hours worked and hourly rate. */
+/*To the already defined EMPLOYEE1 class, add two subclasses. FullTimeEmp and PartTimeEmp. 
+Add the following data Members and Member Functions to the PartTimeEmp class. 
+  hoursWorked(int): Represents the number of hours worked by the part-time employee. 
+  hourly-rate(double, static and final): Represents the hourly rate at which the part-time employee is paid. 
+  The calculateSalary(): overrides the calculateSalary() method of the base class to calculate the salary of the part-time employee 
+                         based on the hours worked and hourly rate. 
+  The displayEmployeeDetails(): Overrides the displayEmplyeeDetails() methods of the base class to display the part-time employee's details, 
+  including the hours worked and hourly rate. */
 
 // Base class EMPLOYEE1
 // Base class EMPLOYEE1
